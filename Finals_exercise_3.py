679d76ef342c39976e38cf4be549148dc8c40e93
ckdev-ui <caseres.christiankenth52@gmail.com>
2026-04-27 02:56:00 +0800

    Add exercise 3

---
+++ Finals_exercise_3.py
@@ -0,0 +1,27 @@
+try:
+    username = input("Enter username: ")
+    age = int(input("Enter age: "))  
+
+    if username.strip() == "" or age <= 0:
+        raise ValueError("Invalid input.")
+
+    with open("users.txt", "a") as file:
+        file.write(f"{username} - {age}\n")
+
+    print("\nUser saved successfully.\n")
+
+except ValueError:
+    print("Error: Please enter a valid name and a positive number for age.")
+
+except Exception as e:
+    print("Unexpected error:", e)
+
+try:
+    print("\nSaved Users:")
+    with open("users.txt", "r") as file:
+        print(file.read())
+
+except FileNotFoundError:
+    print("\nNo users found yet.")
+
+print("System complete.")
